8b707abf4fa28c09a7ea9800c64155fd7410b618
niaim2020 <nyaim2020@gmail.com>
2020-03-22 18:15:28 +0300

    Добавил задачу 17: Запись данных в файл

---
+++ src/DataRecord.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class DataRecord {
+    public static void main(String args[]) {
+        String str;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Чтобы остановить запись введите 'стоп' в новой строке");
+
+        try (FileWriter fw = new FileWriter("file2.txt")) {
+            do {
+                System.out.print(": ");
+                str = br.readLine();
+
+                if(str.compareTo("стоп") == 0) break;
+
+                str = str + "\r\n";
+                fw.write(str);
+            } while (str.compareTo("стоп") != 0);
+        } catch (IOException exc) {
+            System.out.println("Ошибка ввода-вывода: " + exc);
+        }
+    }
+}
